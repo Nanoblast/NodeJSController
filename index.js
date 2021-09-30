cd8f2dd4be5e1fd6a85ba08f17209d1c6eec6d09
Nanoblast <koxigen333@gmail.com>
2021-09-30 22:58:34 +0200

    Static pages and express application

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use (express.static('static'));
+
+const server = app.listen(3000, function() {
+    console.log("On port 3000 POGGERS");
+})
